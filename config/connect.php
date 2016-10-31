561ea0d45f6082b5470b5e6103f04506804c8b24
Phiphat Yoiphosai <rubjang@gmail.com>
2016-10-31 11:07:27 +0700

    no message

--- config/connect.php
+++ config/connect.php
@@ -3,6 +3,7 @@ $ServerName="localhost";
 $UserServer="root";
 $PassServer="1q2w3e4r";
 $DataBase="krutkul_db";
+// database
 
 @$conn=mysql_connect($ServerName,$UserServer,$PassServer) or die("No Connect");
 @mysql_select_db($DataBase,$conn) or die("No Select DB");
