b1bf2a941ccafbd796f70329fa484b2146df48ba
root <root@docker-rb.server>
2016-10-31 08:20:34 +0000

    test ingnore

--- config/connect.php
+++ config/connect.php
@@ -5,6 +5,8 @@ $PassServer="1q2w3e4r";
 $DataBase="krutkul_db";
 // database
 
+
+
 @$conn=mysql_connect($ServerName,$UserServer,$PassServer) or die("No Connect");
 @mysql_select_db($DataBase,$conn) or die("No Select DB");
 mysql_query("SET NAMES UTF8");
