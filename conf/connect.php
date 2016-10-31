dd5e7dd15c49372227a05da4dfd5a45af3005da9
Phiphat Yoiphosai <rubjang@gmail.com>
2016-10-31 10:27:53 +0700

    update .gitignore, config

--- conf/connect.php
+++ conf/connect.php
@@ -1,8 +1,8 @@
 <?php
 $ServerName="localhost";
 $UserServer="root";
-$PassServer="go9io4kgo9ifu";
-$DataBase="krutkul";
+$PassServer="1q2w3e4r";
+$DataBase="krutkul_db";
 
 @$conn=mysql_connect($ServerName,$UserServer,$PassServer) or die("No Connect");
 @mysql_select_db($DataBase,$conn) or die("No Select DB");
