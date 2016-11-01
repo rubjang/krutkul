49021c6715b198290cdd41a376d96ff7a2fc4c56
Phiphat Yoiphosai <rubjang@gmail.com>
2016-11-01 11:55:25 +0700

    no message

--- webboard.php
+++ webboard.php
@@ -9,39 +9,7 @@
     @include('config/connect.php');
     @include('incs/header-top.php');
     //include('config/connect.php');
-      $strSQL = "SELECT * FROM shop_webboard ";
-
-      $objQuery = mysql_query($strSQL) or die ("Error Query");
-      $Num_Rows = mysql_num_rows($objQuery);
-
-      $Per_Page = 10;   // Per Page
-
-      $Page = $_GET["Page"];
-      if(!$_GET["Page"])
-      {
-        $Page=1;
-      }
-
-      $Prev_Page = $Page-1;
-      $Next_Page = $Page+1;
-
-      $Page_Start = (($Per_Page*$Page)-$Per_Page);
-      if($Num_Rows<=$Per_Page)
-      {
-        $Num_Pages =1;
-      }
-      else if(($Num_Rows % $Per_Page)==0)
-      {
-        $Num_Pages =($Num_Rows/$Per_Page) ;
-      }
-      else
-      {
-        $Num_Pages =($Num_Rows/$Per_Page)+1;
-        $Num_Pages = (int)$Num_Pages;
-      }
-
-      $strSQL .=" order by id_webboard desc LIMIT $Page_Start , $Per_Page";
-      $objQuery  = mysql_query($strSQL);
+    
     ?>
     <!-- /Top Head -->
     <script>
